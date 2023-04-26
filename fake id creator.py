4466c01dcde88f5dda08b4d4363353d0fe32ab98
Suvrat Patel <72069889+MadJokkerr@users.noreply.github.com>
2023-04-26 20:27:57 +0530

    Add files via upload

---
+++ fake id creator.py
@@ -0,0 +1,16 @@
+fake id creator
+
+#install faker module
+#import faker module 
+from faker import Faker
+fake = Faker()
+print(fake.name())
+print(fake.email())
+print(fake.country())
+print(fake.text())
+
+print(fake.profile())
+
+
+
+Query for Github : https://github.com/issues?q=is%3Aopen+is%3Aissue+label%3AHacktoberfest+sort%3Aupdated
